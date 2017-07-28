da03d1b84b71afd87d289c82f278a682509d95b5
Krystian Rosiński <krystian.rosinski@gmail.com>
2017-07-28 14:06:02 +0200

    Use a context manager for configuration

--- marker/__init__.py
+++ marker/__init__.py
@@ -10,27 +10,27 @@ from pyramid.threadlocal import get_current_request
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    config = Configurator(settings=settings)
-    session_factory = SignedCookieSessionFactory(settings['session.secret'])
-    config.set_session_factory(session_factory)
-    config.include('.models')
-    config.include('.routes')
-    config.include('.security')
-    deform.renderer.configure_zpt_renderer()
+    with Configurator(settings=settings) as config:
+        session_factory = SignedCookieSessionFactory(settings['session.secret'])
+        config.set_session_factory(session_factory)
+        config.include('.models')
+        config.include('.routes')
+        config.include('.security')
+        deform.renderer.configure_zpt_renderer()
 
-    config.add_translation_dirs(
-        'colander:locale',
-        'deform:locale',
-    )
+        config.add_translation_dirs(
+            'colander:locale',
+            'deform:locale',
+        )
 
-    def translator(term):
-        return get_localizer(get_current_request()).translate(term)
+        def translator(term):
+            return get_localizer(get_current_request()).translate(term)
 
-    deform_template_dir = resource_filename('deform', 'templates/')
-    zpt_renderer = deform.ZPTRendererFactory(
-        [deform_template_dir],
-        translator=translator)
-    deform.Form.set_default_renderer(zpt_renderer)
+        deform_template_dir = resource_filename('deform', 'templates/')
+        zpt_renderer = deform.ZPTRendererFactory(
+            [deform_template_dir],
+            translator=translator)
+        deform.Form.set_default_renderer(zpt_renderer)
 
-    config.scan()
-    return config.make_wsgi_app()
+        config.scan()
+        return config.make_wsgi_app()
