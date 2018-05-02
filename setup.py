626fe052eb2db6f75b2ecacc5775ad129d4557eb
Krystian Rosiński <krystian.rosinski@gmail.com>
2018-05-02 18:35:02 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ tests_require = [
     ]
 
 setup(name='marker',
-      version='0.7',
+      version='0.8',
       description='marker',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
